d988e37463387b5ca4616d30a8a6d426432f4046
Lukas Bang Stoltz-Andersen <lukasbangstoltz@gmail.com>
2020-11-10 15:47:52 +0100

    kan nu fetche fra flere endpoints

--- src/main/java/rest/StarWarsResource.java
+++ src/main/java/rest/StarWarsResource.java
@@ -4,6 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import utils.EMF_Creator;
 import facades.FacadeExample;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,25 +15,36 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
-public class RenameMeResource {
+@Path("starwars")
+public class StarWarsResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-       
-    private static final FacadeExample FACADE =  FacadeExample.getFacadeExample(EMF);
+    private static final ExecutorService ES = Executors.newCachedThreadPool();
+    private static final FacadeExample FACADE = FacadeExample.getFacadeExample(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-            
+    
+
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String demo() {
         return "{\"msg\":\"Hello World\"}";
     }
+
     @Path("count")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String getRenameMeCount() {
         long count = FACADE.getRenameMeCount();
         //System.out.println("--------------->"+count);
-        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+        return "{\"count\":" + count + "}";  //Done manually so no need for a DTO
     }
+
+    @Path("parrallel")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getStarWarsParrallel() throws InterruptedException, ExecutionException, TimeoutException {
+        String result = fetcher.StarWarsFetcher.responseFromExternalServersParrallel(ES, GSON);
+        return result;
+    }
+
 }
