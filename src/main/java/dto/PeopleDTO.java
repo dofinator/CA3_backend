173600d0f35581fdc15c8d5d8ee239cb790df3a7
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2020-11-10 13:15:52 +0100

    added people dto

---
+++ src/main/java/dto/PeopleDTO.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author seb
+ */
+public class PeopleDTO {
+    private String name;
+    private String height;
+    private String hairColor;
+    private String eyeColor;
+    private String gender;
+
+    public PeopleDTO() {
+    }
+
+    public PeopleDTO(String name, String height, String hairColor, String eyeColor, String gender) {
+        this.name = name;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+}
