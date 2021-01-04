fbf1e3ebfabe683856f48b799314ef64e884a28d
Sumit Dey <sumitdey0007@gmail.com>
2021-01-04 15:45:23 +0100

    håndtering af lister er lavet

---
+++ src/main/java/dto/CountriesDTO.java
@@ -0,0 +1,17 @@
+package dto;
+
+/**
+ *
+ * @author sebas
+ */
+public class CountriesDTO {
+    
+    public String Country;
+    public String Slug;
+
+    public CountriesDTO(String Country, String Slug) {
+        this.Country = Country;
+        this.Slug = Slug;
+    }
+
+}
